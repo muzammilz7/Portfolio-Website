e65fc4d5dc4d9b33fd06c0695d3d2a30f54246ee
muzammilz7 <133916565+muzammilz7@users.noreply.github.com>
2023-10-23 16:30:09 -0400

    changed hero section

--- src/pages/Home/About.jsx
+++ src/pages/Home/About.jsx
@@ -29,9 +29,10 @@ export default function About() {
       <section id="about_sec" className="about_class">
         <div className="about_sec_info">
           <div className="about_hero">
-            <h1 className="about_content">About Me 1</h1>
+            <h1 className="about_content">About Me </h1>
             <p className="about_par">
-           1
+            I'm a dedicated Software Developer with a strong passion for web development and cybersecurity. I thrive on crafting user-friendly web applications and enhancing digital security. 
+          My journey involves continuous learning and exploration to make the digital world safer and more accessible. 
             </p>
             <h2 className="about_interests">Interests:</h2>
             <ul className="about_skills">
